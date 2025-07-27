c8245add3fff34a956bab03ac45ce8affedab866
Giovanna Moura <giovm14@gmail.com>
2025-07-27 20:29:01 -0300

    feat: adiciona projeto django wtads

---
+++ wtads2025/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Wtads2025Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wtads2025'
---
+++ wtads2025/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def wtads2025(request):
+    return render(request, 'wtads2025/index.html')
